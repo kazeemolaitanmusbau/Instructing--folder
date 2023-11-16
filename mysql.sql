e684dd0bbdfecbe569ad5ea9979b4d1d8fa231ab
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-16 13:10:28 +0200

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -69,3 +69,62 @@ VALUES(1, "yemi", "kemi", '10 lekki', "Chemistry"),
 		(4, 'Rashford', 'sancho', 'agege', "physics"),
 		(5, 'holar', 'Richael', '44 magodo', "chemical"),
 		(6, 'kola', 'Ada', '12 berger', "Electrical");
+        
+        
+SELECT * FROM student_info;
+
+INSERT INTO student_info(firstname, department)
+VALUES ("Ada", "Agric");
+
+
+SELECT * FROM student_info;
+
+/*
+constrainst are set of rules impose on the columns to ensure data integrity
+
+not null
+default
+check
+unique
+enum
+priamry key
+auto increment
+foreign key
+*/
+
+CREATE TABLE T1 (
+id int NOT NULL,
+firstname varchar(10)
+);
+
+INSERT INTO T1()
+VALUES(1, "KOLA");
+
+SELECT * FROM T1;
+
+INSERT INTO T1(firstname)
+VALUES("yemi");
+
+
+
+
+INSERT INTO T1(id)
+VALUES(2);
+
+
+CREATE TABLE t2(
+firstname varchar(20),
+state char(10) DEFAULT "lagos"
+);
+
+INSERT INTO t2(firstname, state)
+VALUES ('KOLA', "KANO"),
+		("YEMI", "KADUNA");
+        
+SELECT * FROM t2;
+
+INSERT INTO t2(firstname)
+VALUES ("olaitan"),
+		("sancho");
+        
+   SELECT * FROM t2;     

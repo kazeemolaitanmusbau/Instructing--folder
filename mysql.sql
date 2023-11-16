a617418f911c9b6a2ec1c38825d9a52c34a4d31e
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-16 10:23:36 +0200

    Create mysql.sql

---
+++ mysql.sql
@@ -0,0 +1,23 @@
+-- comment/ statement
+
+-- introduction to sql
+/*
+this
+is
+multple 
+line 
+comment
+*/
+
+/*
+DDL: Dadta Defination Language------ CREATE, ALTER, DROP
+
+DML: Data Manipulation language----- SELECT, DELETE, UPDATE, INSERT
+
+
+DCL: Data Control language--- grant, revoke
+
+*/
+
+
+

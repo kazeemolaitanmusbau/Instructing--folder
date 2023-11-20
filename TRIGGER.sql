cd9c055a7367aa768935eb8f5709b7c6e519abea
kazeem musbau olaitan <kazeemmusbau@gmail.com>
2023-11-20 14:03:56 +0200

    Update TRIGGER.sql

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -1,10 +1,17 @@
+/*
+
+In MySQL, a trigger is a stored program invoked automatically in response to an event such as insert, update, or delete that occurs in the associated table. 
+For example, you can define a trigger that is invoked automatically before a new row is inserted into a table.
+*/
+
+
 SELECT * FROM company.pets;
 
 
 CREATE TABLE employee_audit(
 OwnerID INT ,
 Name VARCHAR(20) NOT NULL,
-kind VARCHAR(20),
+kind VARCHAR(20),						-- TABLE TO RECORD THE DEFINED FEATURE WHEN PET TABLE TRIGGER ON UPDATE 
 changedate DATETIME DEFAULT NULL,
 action VARCHAR(50) DEFAULT NULL );
 
@@ -16,7 +23,7 @@ CREATE TRIGGER pet_update
 AFTER UPDATE ON pets FOR EACH ROW
 INSERT INTO employee_audit
 SET action = 'ola update',
-OwnerID = OLD.OwnerID,
+OwnerID = OLD.OwnerID,			
 Name = OLD.Name,
 kind = OLD.kind,
 changedate = NOW();
